f560dc6175c196b51ef109bbdc3538850860a6ca
Lisandro Etcheverry <lism.etcheverry@gmail.com>
2026-05-10 16:30:00 -0300

    fix: remove BOM from Lua files to prevent PZ startup crash

---
+++ 42/media/lua/shared/TM_TrueMooZicPatricioReyYRedonditos_SandboxVars.lua
@@ -0,0 +1,12 @@
+if SandboxVars == nil then SandboxVars = {} end
+if SandboxVars["TM_TrueMooZicPatricioReyYRedonditos"] == nil then
+    SandboxVars["TM_TrueMooZicPatricioReyYRedonditos"] = {
+        BuildingSpawnMultiplier = 1.0,
+        VehicleSpawnMultiplier = 1.0,
+        ZombieSpawnMultiplier = 1.0,
+    }
+end
+
+if SandboxVars["TM_TrueMooZicPatricioReyYRedonditos"].BuildingSpawnMultiplier == nil then SandboxVars["TM_TrueMooZicPatricioReyYRedonditos"].BuildingSpawnMultiplier = 1.0 end
+if SandboxVars["TM_TrueMooZicPatricioReyYRedonditos"].VehicleSpawnMultiplier == nil then SandboxVars["TM_TrueMooZicPatricioReyYRedonditos"].VehicleSpawnMultiplier = 1.0 end
+if SandboxVars["TM_TrueMooZicPatricioReyYRedonditos"].ZombieSpawnMultiplier == nil then SandboxVars["TM_TrueMooZicPatricioReyYRedonditos"].ZombieSpawnMultiplier = 1.0 end
